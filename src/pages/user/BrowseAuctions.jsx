5568855df012ec96aff4542f22dec8457269e66f
Austin <95069137+AUSTIN0022@users.noreply.github.com>
2025-06-11 14:57:34 +0530

    upcoming

--- src/pages/user/BrowseAuctions.jsx
+++ src/pages/user/BrowseAuctions.jsx
@@ -63,9 +63,12 @@ const BrowseAuctions = () => {
     });
 
   const getStatusBadge = (status) => {
+    
+    if(status == "pending") status = "upcoming";
+    
     const statusConfig = {
       active: { color: 'bg-green-100 text-green-800 border-green-200', icon: Clock },
-      pending: { color: 'bg-yellow-100 text-blue-800 border-blue-200', icon: Calendar },
+      upcomimg: { color: 'bg-yellow-100 text-yellow-800 border-yellow-200', icon: Calendar },
       completed: { color: 'bg-gray-100 text-gray-800 border-gray-200', icon: Eye }
     };
     
